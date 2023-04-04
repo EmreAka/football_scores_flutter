5d6f438a885b72183f049a7cff6d6f5c2030bfcc
Emre <aka548548@gmail.com>
2023-04-04 22:56:55 +0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(appBarTheme: const AppBarTheme(backgroundColor: Colors.black87)),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
