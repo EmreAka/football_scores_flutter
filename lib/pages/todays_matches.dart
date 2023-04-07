7f2797ad37fde9381fe9197e6ed40dc398499cb8
Emre <aka548548@gmail.com>
2023-04-08 00:07:50 +0300

    todaysmatch

--- lib/pages/todays_matches.dart
+++ lib/pages/todays_matches.dart
@@ -3,97 +3,135 @@ import 'package:football_scores/models/selected_route.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:football_scores/widgets/navigation_drawer.dart' as NavigationDrawer;
 
-class TodaysMatches extends StatelessWidget {
-  TodaysMatches({super.key});
+import '../services/live_score_service.dart';
 
-  final pb = PocketBase('https://gofootball.emreaka.tech/');
+class TodaysMatches extends StatelessWidget {
+  const TodaysMatches({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavigationDrawer.NavigationDrawer(selectedRoute: SelectedRoute.todaysMatches,),
+      drawer: NavigationDrawer.NavigationDrawer(selectedRoute: SelectedRoute.todaysMatches),
       appBar: AppBar(
         title: const Text("Today's Matches"),
         backgroundColor: Colors.black87,
         
       ),
-      body: Matches(pb: pb),
+      body: Matches(),
     );
     
   }
 }
 
 class Matches extends StatefulWidget {
-  final PocketBase pb;
-
-  const Matches({super.key, required this.pb});
+  const Matches({super.key});
 
   @override
-  State<StatefulWidget> createState() => _MatchesState(pb);
+  State<StatefulWidget> createState() => _MatchesState();
 }
 
 class _MatchesState extends State {
-  final PocketBase pb;
-  late Future<dynamic> result;
+  late Future<List<dynamic>> todayEvents;
 
-  _MatchesState(this.pb);
+  _MatchesState();
 
   @override
   void initState() {
     super.initState();
-    result = pb.send("/todaysmatches");
+    todayEvents = LiveScoreService().getTodaysEvents();
   }
 
   @override
-  Widget build(BuildContext context) => FutureBuilder(
-        future: result,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Column(
-                    children: [
-                      Container(
-                        color: Colors.black87,
-                        child: ListTile(
+  Widget build(BuildContext context) => RefreshIndicator(
+      onRefresh: () async {
+        var result = await LiveScoreService().getLiveScores();
+        setState(() {
+          todayEvents = Future<List<dynamic>>.value(result);
+        });
+      },
+      child: FutureBuilder(
+          future: todayEvents,
+          builder: ((context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        ListTile(
+                          tileColor: Colors.black87,
                           title: Text(
-                            snapshot.data[index]['country_name_en'],
+                            "${snapshot.data![index]['country']?['countryNameEnglish'] ?? "Unknown"} - ${snapshot.data![index]['leagueName']}",
                             style: const TextStyle(color: Colors.white),
                           ),
+                          leading: Image.network(
+                            snapshot.data![index]['country']
+                                    ?['countryLogoUrl'] ??
+                                "https://apiv3.apifootball.com/badges/logo_country/2_intl.png",
+                            height: 30,
+                            width: 40,
+                          ),
                         ),
-                      ),
-                      ListView.builder(
-                        itemCount: snapshot.data[index]['matches'].length,
-                        itemBuilder: (BuildContext context, int indexx) {
-                          return Container(
-                            color:
-                                indexx % 2 == 0 ? Colors.white : Colors.black12,
-                            child: ListTile(
-                              title: Text(
-                                snapshot.data[index]['matches'][indexx]
-                                        ['home_team'] +
-                                    " - " +
-                                    snapshot.data[index]['matches'][indexx]
-                                        ['away_team'],
-                              ),
-                              trailing: IconButton(icon: const Icon(Icons.notification_add),onPressed: (){}),
-                              subtitle: snapshot.data[index]['matches'][indexx]
-                              ['match_live'] != "1" ? Text("Starts at: ${snapshot.data[index]['matches'][indexx]
-                              ['match_time']}") : Text("Live: ${snapshot.data[index]['matches'][indexx]
-                              ['match_status']}", style: const TextStyle(color: Colors.green, fontWeight: FontWeight.bold),),
-                            ),
-                          );
-                        },
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                      ),
-                    ],
-                  );
-                });
-          } else {
-            return LinearProgressIndicator();
-          }
-        },
-      );
-}
+                        ListView.builder(
+                          itemCount: snapshot.data![index]['matches'].length,
+                          itemBuilder: (context, matchIndex) {
+                            return matchResult(snapshot, index, matchIndex);
+                          },
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                        )
+                      ],
+                    );
+                  });
+            } else {
+              return const LinearProgressIndicator();
+            }
+          })),
+    );
+  }
+
+  Widget matchResult(
+      AsyncSnapshot<List<dynamic>> snapshot, int index, int matchIndex) {
+    var color = matchIndex % 2 != 0 ? Color.fromARGB(255, 235, 235, 235) : Colors.white;
+    var isLive = snapshot.data![index]['matches'][matchIndex]['match_live'] == "1" ? true : false;
+    return Container(
+      color: color,
+      child: Padding(
+        padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+        child: SizedBox(
+          height: 50,
+          child: Row(children: [
+            SizedBox(
+              width: 60,
+              child: Text(
+                "${snapshot.data![index]['matches'][matchIndex]['match_status']}'",
+                style: TextStyle(color: isLive ? Colors.green : Colors.black),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 30),
+                child: Text(
+                  "${snapshot.data![index]['matches'][matchIndex]['match_hometeam_name']}",
+                  textAlign: TextAlign.end,
+                ),
+              ),
+            ),
+            Text(
+                "${snapshot.data![index]['matches'][matchIndex]['match_hometeam_score']} - ${snapshot.data![index]['matches'][matchIndex]['match_awayteam_score']}",
+                style: const TextStyle(fontWeight: FontWeight.bold),),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Text(
+                    "${snapshot.data![index]['matches'][matchIndex]['match_awayteam_name']}"),
+              ),
+            ),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.notification_add))
+          ]),
+        ),
+      ),
+    );
+  }
--- lib/services/live_score_service.dart
+++ lib/services/live_score_service.dart
@@ -13,4 +13,16 @@ class LiveScoreService {
       throw Exception("Can't fetch");
     }
   }
+
+  Future<List<dynamic>> getTodaysEvents() async {
+    final url = Uri.parse("http://localhost:5141/api/v1/events/today");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print((convert.jsonDecode(response.body)as List<dynamic>).length);
+      print((convert.jsonDecode(response.body)as List<dynamic>)[0]['matches'].length);
+      return convert.jsonDecode(response.body);
+    }else{
+      throw Exception("Can't fetch");
+    }
+  }
 }
